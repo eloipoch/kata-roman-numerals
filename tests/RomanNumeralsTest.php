a9b230d633f5490b42f2408331e7332b7a4572cb
Eloi Poch <eloi.poch@gmail.com>
2014-02-22 19:09:02 +0100

    add test to arabic number 3

--- tests/RomanNumeralsTest.php
+++ tests/RomanNumeralsTest.php
@@ -30,9 +30,10 @@ class RomanNumeralsTest extends PHPUnit_Framework_TestCase
     public static function provideArabicNumbersWithItsExpectations()
     {
         return [
-            '0' => ['expectedRomanNumber' => '',   'arabicNumber' => 0],
-            '1' => ['expectedRomanNumber' => 'I',  'arabicNumber' => 1],
-            '2' => ['expectedRomanNumber' => 'II', 'arabicNumber' => 2],
+            '0' => ['expectedRomanNumber' => '',    'arabicNumber' => 0],
+            '1' => ['expectedRomanNumber' => 'I',   'arabicNumber' => 1],
+            '2' => ['expectedRomanNumber' => 'II',  'arabicNumber' => 2],
+            '3' => ['expectedRomanNumber' => 'III', 'arabicNumber' => 3],
         ];
     }
 }
